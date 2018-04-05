e44cd52952908db4aad6680ee367f805ded1531e
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-04-05 23:40:57 +0200

    add ofert

--- src/containers/AddOffert.js
+++ src/containers/AddOffert.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import Button from 'material-ui/Button';
+import { connect } from 'react-redux';
+import { renderTextField } from '../helpers/reduxFormField';
+import { compose } from '../helpers/compose';
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Please enter an email.";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = "Please enter a password.";
+  }
+
+  if (!values.ofertDescription) {
+    errors.ofertDescription = "Ofert description is required";
+  }
+
+  return errors;
+};
+
 
 class AddOffert extends Component {
+
+  addOfert = (values) => {
+
+  };
+
   render() {
-    return (
-      <div>Add offert private</div>
+    const { handleSubmit } = this.props;
+    return(
+      <div className="container">
+        <div className="">
+          <h2 className="">Add ofert</h2>
+          <form onSubmit={handleSubmit(this.addOfert)}>
+            <Field name="email" component={renderTextField} className="" type="text" label="Email"/>
+            <Field multiline rows="6" name="ofertDescription" component={renderTextField} className="" type="text" label="Ofert description"/>
+            <Button variant="raised" type="submit" color="primary"  className="button-submit">
+              Add
+            </Button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
 
-export default AddOffert;
+export default compose(
+  connect(null, { }),
+  reduxForm({
+    form: 'addOffert',
+    validate
+  })
+)(AddOffert);
