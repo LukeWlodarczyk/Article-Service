86ae41a054ac175cafebe30dc7a81b57d9992dc9
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-04-17 23:53:56 +0200

    create article

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -8,3 +8,12 @@ export const doCreateUser = (id, username, email) =>
 
 export const onceGetUsers = () =>
   db.ref('users').once('value');
+
+export const doCreateArticle = (id, title, body) =>
+  db.ref(`articles/${id}`).set({
+    title,
+    body,
+  });
+
+export const onceGetArticles = () =>
+  db.ref('articles').once('value');
